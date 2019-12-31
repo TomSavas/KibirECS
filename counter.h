f1608dbae49d99808b907ab19cbd7d4d0fc7e3ae
Sawas <tomas.savickas@protonmail.com>
2019-12-31 19:24:11 +0000

    Remove redundant assert

--- counter.h
+++ counter.h
@@ -5,7 +5,6 @@
 namespace KibirECS {
     template<typename T>
     struct Counter {
-        //static_assert(std::is_integral<T>::value, "Counter can only be used with integral types.");
         static int value;
     };
 
